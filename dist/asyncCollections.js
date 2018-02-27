ff28b9ae9ed021062bf9f487e6395a962c87c8bf
Tristan <hulot.tristan@gmail.com>
2018-02-27 23:44:26 +0100

    remove dist folder

--- dist/asyncCollections.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = ['each', 'eachSeries', 'eachLimit', 'map', 'mapSeries', 'mapLimit', 'filter', 'filterSeries', 'reject', 'rejectSeries', 'reduce', 'reduceRight', 'detect', 'detectSeries', 'sortBy', 'some', 'every', 'concat', 'concatSeries'];
